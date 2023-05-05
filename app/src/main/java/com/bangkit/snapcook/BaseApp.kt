afdc49623681f6bf10891315616a8bcb828fe661
Ibnu Batutah <ibnu@rollingglory.com>
2023-05-06 01:10:21 +0700

    Test Minor Change

--- app/src/main/java/com/bangkit/snapcook/BaseApp.kt
+++ app/src/main/java/com/bangkit/snapcook/BaseApp.kt
@@ -9,7 +9,6 @@ import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import timber.log.Timber
-
 class BaseApp : Application() {
     override fun onCreate() {
         super.onCreate()
